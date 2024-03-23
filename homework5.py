9c62bf63514d01873b5bef1766ae9e749a6ccbb2
IbnKamil <163489394+IbnKamil@users.noreply.github.com>
2024-03-23 15:17:35 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+
+# Работа со списками
+my_list = ['Apple', 'Milk', 'Potato', 'Banana', 'Blackberry']
+print(my_list)
+print(my_list[0:5:4])                #print((my_list[0]), (my_list[-1]))
+print(my_list[2:5])
+my_list [2] = 'Cherry'
+print(my_list)
+
+# Работа со словарями
+my_dict = {'Apple': 'Яблоко', 'Milk': "Молоко", 'Cherry': "Вишня",'Banana': "Банан",'Blackberry': "Ежевика"}
+print(my_dict)
+print(my_dict['Cherry'])             #print(my_dict.get('Cherry'))
+my_dict ['Fish'] = 'Рыба'
+print(my_dict)
